624f97afc76d653538667556dab3e346e30a1d06
paulguilloux-opencell <paul.guilloux@opencellsoft.com>
2022-09-29 16:31:06 +0200

    Run tests on file save

--- runner/models.py
+++ runner/models.py
@@ -1,4 +1,7 @@
+import json
 from django.db import models
+import requests
+
 
 # Create your models here.
 class Exercise(models.Model):
@@ -13,7 +16,7 @@ class Exercise(models.Model):
 
 class Test(models.Model):
     exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
-    rules = models.JSONField()
+    rules = models.TextField()
     
 class Submission(models.Model):
     """
@@ -26,12 +29,56 @@ class Submission(models.Model):
     def __str__(self):
         return self.file.name
 
+    def save(self, *args, **kwargs):
+
+        super(Submission, self).save(*args, **kwargs)
+
+        url = "http://oasis:1234/run"
+        submission = self
+        filename = submission.file.path
+        #tests_object = json.loads(exercise.tests)
+        test_objects = [dic["rules"] for dic in list(Test.objects.all().values('rules'))]
+
+        test_object = json.loads(test_objects[0])
+
+        data = {}
+        data['lang'] = 'python'
+        data.update(test_object) #only run the first test : this is temporary !
+        with open(filename, 'r') as f:
+            data['source'] = f.read()
+
+        r = requests.post(url, json=data)
+        camisole_response = json.loads(r.text)
+
+        test_results = []
+
+        for test in camisole_response["tests"]:
+            expected_stdout = next(initial_test for initial_test in test_object["tests"] if initial_test["name"] == test["name"])["stdout"]
+            test_results.append(test)
+
+            existing_result = TestResult.objects.filter(submission = submission, exercise_test = Test.objects.all()[0])
+
+            test_result = TestResult(
+                id = existing_result.first().id if existing_result else None,
+                submission = submission,
+                exercise_test = Test.objects.all()[0],
+                stdout = test["stdout"],
+                success = test["stdout"] == expected_stdout,
+                time = round(test["meta"]["wall-time"],2),
+                memory = test["meta"]["cg-mem"]
+            )
+            test_result.save()
+
 class TestResult(models.Model):
     """
     A test, ran on a file
     """
-    file = models.ForeignKey(Submission, on_delete=models.CASCADE)
+    submission = models.ForeignKey(Submission, on_delete=models.CASCADE)
     exercise_test = models.ForeignKey(Test, on_delete=models.CASCADE)
+    stdout = models.CharField(max_length=255, default="")
     success = models.BooleanField(default=False)
     time = models.FloatField()
     memory = models.IntegerField()
+
+    class Meta:
+        unique_together = ('submission','exercise_test')
