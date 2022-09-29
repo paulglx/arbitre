39f7f420cdcf0390a9e940cb9eeec3841842a736
paulguilloux-opencell <paul.guilloux@opencellsoft.com>
2022-09-29 16:30:49 +0200

    Include test results in admin for testing

--- runner/admin.py
+++ runner/admin.py
@@ -1,13 +1,17 @@
 from django.contrib import admin
-from .models import Submission, Exercise
+from .models import Submission, Exercise, Test, TestResult
 
 # Register your models here.
 class SubmissionInline(admin.StackedInline):
     model = Submission
     extra = 1
 
+class TestInline(admin.StackedInline):
+    model = Test
+    extra = 1
 class ExerciseAdmin(admin.ModelAdmin):
-    fields = ['title','statement','tests']
-    inlines = [SubmissionInline]
+    fields = ['title','statement']
+    inlines = [TestInline, SubmissionInline]
 
 admin.site.register(Exercise, ExerciseAdmin)
+admin.site.register(TestResult)
