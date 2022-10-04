84724799e3cccd26da1dc7c9c84dc891ccd9c715
paulglx <paul.guilloux@telecom-sudparis.eu>
2022-10-04 11:47:23 +0200

    Fix test result string representation

--- runner/models.py
+++ runner/models.py
@@ -1,30 +1,26 @@
 import json
 from django.db import models
 import requests
+from .tasks import run_camisole
 
 # Create your models here.
 class Exercise(models.Model):
     """
     The exercise given to the student
     """
+
     title = models.CharField(max_length=255)
     statement = models.TextField()
 
     def __str__(self):
         return self.title
 
-class Test(models.Model):
-    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
-    name = models.CharField(max_length=255, default="")
-    stdin = models.TextField(default="")
-    stdout = models.TextField(default="")
 
-from .tasks import run_camisole
-    
 class Submission(models.Model):
     """
-    The code file stored that will be judged
+    The stored code file that will be judged
     """
+
     exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
     file = models.FileField(upload_to="fileuploads")
     owner = models.CharField(max_length=255, default="John Doe")
@@ -34,22 +30,33 @@ class Submission(models.Model):
 
     def save(self, *args, **kwargs):
 
-        #Save submission to database
+        # Save submission to database
         super(Submission, self).save(*args, **kwargs)
 
-        tests = Test.objects.filter(exercise = self.exercise)
+        tests = Test.objects.filter(exercise=self.exercise)
 
-        for test in tests :
-            #Add camisole task to queue
-            run_camisole.delay(
-                submission_id = self.id,
-                test_id = test.id
-            )
+        for test in tests:
+            # Add camisole task to queue
+            run_camisole.delay(submission_id=self.id, test_id=test.id)
+
+
+class Test(models.Model):
+
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, default="")
+    stdin = models.TextField(default="")
+    stdout = models.TextField(default="")
+    #TODO add all test criterias
+
+    def __str__(self):
+        return self.name + " (" + str(self.exercise) + ")"
+
 
 class TestResult(models.Model):
     """
     A test, ran on a file
     """
+
     submission = models.ForeignKey(Submission, on_delete=models.CASCADE)
     exercise_test = models.ForeignKey(Test, on_delete=models.CASCADE)
     running = models.BooleanField(default=False)
@@ -59,4 +66,7 @@ class TestResult(models.Model):
     memory = models.IntegerField(default=-1)
 
     class Meta:
-        unique_together = ('submission','exercise_test')
+        unique_together = ("submission", "exercise_test")
+
+    def __str__(self):
+        return self.submission.exercise.title + " : " + self.exercise_test.name + " (" + str(self.submission.owner) + ")"
