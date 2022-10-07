3d7d6f4143aeaef62a9bc75b5378db9175884d8f
paulglx <paul.guilloux@telecom-sudparis.eu>
2022-10-07 13:55:14 +0200

    Setup Users API access point

--- backend/arbitre/urls.py
+++ backend/arbitre/urls.py
@@ -15,15 +15,22 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from auth.views import UserViewSet
 
+from rest_framework import routers
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
+#Auth router
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("runner/", include('runner.urls')),
+    path('admin/', admin.site.urls),
+    path('runner/', include('runner.urls')),
+    path('api/auth/', include(router.urls)),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
