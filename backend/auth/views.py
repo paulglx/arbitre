6bda930a9b8d5a5f52f8ad8d296182e19408d8d5
paulguilloux-opencell <paul.guilloux@opencellsoft.com>
2022-10-07 13:55:14 +0200

    Setup Users API access point

---
+++ backend/auth/views.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import User
+from rest_framework import viewsets, serializers
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'is_staff']
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
