d0fea27a1473b8b4c870970c98d3cb6a6ca6b8a3
paulglx <paul.guilloux@telecom-sudparis.eu>
2022-10-10 10:03:43 +0200

    Add React Router dependency

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { AuthProvider } from './context/authProvider';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
