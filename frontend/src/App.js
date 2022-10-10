f4f9c25c2aee5229c3ba4a4e9c109d1d62204276
paulglx <paul.guilloux@telecom-sudparis.eu>
2022-10-10 10:24:14 +0200

    Add Routing to main page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,30 @@
-import Register from './Register'
-import Login from './Login'
+import Home from './components/Home';
+import Layout from './components/Layout';
+import Login from './components/Login';
+import Missing from './components/Missing';
+import Register from './components/Register';
+import Unauthorized from './components/Unauthorized';
+
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <main className='App'>
-      <Login />
-      <hr />
-      <Register />
-    </main>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+
+        {/* Public routes */}
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="unauthorized" element={<Unauthorized />} />
+
+        {/* Protected routes */}
+        <Route path="/" element={<Home />} />
+
+        {/* Catch all */}
+        <Route path="*" element={<Missing />} />
+
+      </Route>
+    </Routes>
   );
 }
 
