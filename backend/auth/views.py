cf117e2054d0408584312ae172a4cc83d18f766a
paulglx <paul.guilloux@telecom-sudparis.eu>
2022-10-10 09:59:51 +0200

    Create register user API endpoint with password hashing

--- backend/auth/views.py
+++ backend/auth/views.py
@@ -1,11 +1,24 @@
 from django.contrib.auth.models import User
-from rest_framework import viewsets, serializers
+from rest_framework import viewsets, serializers, permissions
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
+
+    password = serializers.CharField(write_only=True)
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password'],
+        )
+
+        return user
+
     class Meta:
         model = User
-        fields = ['url', 'username', 'password', 'email', 'is_staff']
+        fields = ['url','id', 'username', 'password']
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
+    permission_classes = [permissions.AllowAny]
     serializer_class = UserSerializer
+
