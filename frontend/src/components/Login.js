6a925ede276a9e9dceeebe9c4da19de3614513f1
paulguilloux-opencell <paul.guilloux@opencellsoft.com>
2022-10-10 10:24:01 +0200

    Add Pages

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,6 +1,6 @@
 import { useRef, useState, useEffect, useContext } from "react";
-import AuthContext from "./context/authProvider";
-import axios from './api/axios'
+import AuthContext from "../context/authProvider";
+import axios from '../api/axios'
 
 const LOGIN_URL = '/api/auth/token/'
 
