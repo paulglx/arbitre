69551ac1856ef412c087fad721ca967cec53943b
paulguilloux-opencell <paul.guilloux@opencellsoft.com>
2022-10-11 18:17:04 +0200

    Fix error pages (404 and unauthorized)

---
+++ frontend/src/components/Missing.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Missing = () => {
+  return (
+    <div>
+      <h2>
+        404 - Missing
+      </h2>
+      <p>
+        The page you are looking for doesn't semm to exist.
+      </p>
+    </div>
+  )
+}
+
+export default Missing;
