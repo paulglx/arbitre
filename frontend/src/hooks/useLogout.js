61dd5279fc47b2ed9785ca7c6f455da0cb9677d8
paulglx <paul.guilloux@telecom-sudparis.eu>
2022-10-13 15:07:21 +0200

    Add logout API endpoint, and logout button on website

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -1,15 +1,20 @@
 import axios from '../api/axios';
 import useAuth from './useAuth';
 
+const LOGOUT_URL = 'api/auth/logout/';
+
 const useLogout = () => {
-    const { setAuth } = useAuth();
+    const { auth, setAuth } = useAuth();
 
     const logout = async () => {
+        console.log("auth:", auth);
+        const refreshToken = auth?.refreshToken;
         setAuth({});
         try {
-            const response = await axios('/logout', {
-                withCredentials: true
-            });
+            const response = await axios.post(
+                LOGOUT_URL,
+                JSON.stringify({refreshToken})
+            );
         } catch (err) {
             console.error(err);
         }
@@ -17,3 +22,5 @@ const useLogout = () => {
 
     return logout;
 }
+
+export default useLogout;
