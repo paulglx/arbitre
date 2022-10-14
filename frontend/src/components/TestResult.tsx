cc393fbb65547e5e77ba22860f7eabb5ea01f9a7
paulglx <paul.guilloux@telecom-sudparis.eu>
2022-10-14 19:34:15 +0200

    Implement file uploading

---
+++ frontend/src/components/TestResult.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TestResult = () => {
+
+    
+
+    return (
+        <div>TestResult</div>
+    )
+}
+
+export default TestResult
--- frontend/src/features/submission/submissionApiSlice.ts
+++ frontend/src/features/submission/submissionApiSlice.ts
@@ -2,12 +2,15 @@ import { apiSlice } from "../../app/api/apiSlice";
 
 export const submissionApiSlice = apiSlice.injectEndpoints({
     endpoints: builder => ({
-        createSubmission: builder.mutation({
-            query: data => ({
-                url: 'runner/api/submission/',
-                method: 'POST',
-                body: { ...data }
-            })
+        createSubmission: builder.mutation<{}, FormData>({
+            query: (data:any) => {
+                console.log(data)
+                return ({
+                    url: 'runner/api/submission/',
+                    method: 'POST',
+                    credentials: 'include',
+                    body: data
+            })}
         }),
     })
 })
