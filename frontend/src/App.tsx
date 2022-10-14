e2a895a82357ae607fccab8dfe39b3e344165373
paulglx <paul.guilloux@telecom-sudparis.eu>
2022-10-14 17:14:19 +0200

    Add exercise, session and course pages to navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import Course from "./components/Course";
 import Layout from "./components/Layout";
 import Login from "./components/Login";
 import Users from "./components/Users";
 import Public from "./components/Public";
 import Register from "./components/Register";
 import RequireAuth from "./components/RequireAuth";
+import Session from "./components/Session";
 import Welcome from "./components/Welcome";
 import Exercise from "./components/Exercise";
 
@@ -18,12 +20,14 @@ function App() {
 				<Route index element={<Public/>} />
 				<Route path="login" element={<Login/>} />
 				<Route path="register" element={<Register/>} />
+				<Route path="exercise/:id" element={<Exercise/>} />
+				<Route path="session/:id" element={<Session/>} />
+				<Route path="course/:id" element={<Course/>} />
 
 				{/* Protected routes */}
 				<Route element={<RequireAuth/>}>
 					<Route path="welcome" element={<Welcome/>} />
 					<Route path="users" element={<Users/>} />
-					<Route path="exercise/:id" element={<Exercise/>} />
 				</Route>
 
 			</Route>
