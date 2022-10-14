53b06ee30c5a7bb38160f1c662222f36a6b584ee
paulguilloux-opencell <paul.guilloux@opencellsoft.com>
2022-10-14 14:15:47 +0200

    Create Exercise page

--- frontend/src/components/Exercise.tsx
+++ frontend/src/components/Exercise.tsx
@@ -1,13 +1,15 @@
-import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 import { useGetExerciseQuery } from "../features/courses/exerciseApiSlice";
+import { useGetSessionQuery } from "../features/courses/sessionApiSlice";
+import { useGetCourseQuery } from "../features/courses/courseApiSlice";
 
-import { Container } from "react-bootstrap";
+import { Container, Navbar, ListGroup } from "react-bootstrap";
 
 const Exercise = () => {
 
     const { id }:any = useParams();
+
     const {
         data: exercise,
         isLoading,
@@ -16,12 +18,43 @@ const Exercise = () => {
         error
     } = useGetExerciseQuery({id});
 
-    console.log(exercise)
+    const {
+        data: session,
+        isLoading: sessionIsLoading,
+        isSuccess: sessionIsSuccess,
+        isError: sessionIsError,
+        error: sessionError
+    } = useGetSessionQuery({id});
 
-    return (
+    const {
+        data: course,
+        isLoading: courseIsLoading,
+        isSuccess: courseIsSuccess,
+        isError: courseIsError,
+        error: courseError
+    } = useGetCourseQuery({id});
+
+    console.log(exercise, session)
+
+    return isLoading || sessionIsLoading || courseIsLoading ? (
+        <p>Loading...</p>
+    ):(
+    <Container>
+        <Navbar bg="light" expand="lg">
+            <Container>
+                <Navbar.Brand href={"/course/"+course.id}>{course.title}</Navbar.Brand> {session.title}
+            </Container>
+        </Navbar>
+
+        <br />
+
         <Container>
-            <Navbar></Navbar>
+            <h1>{exercise.title}</h1>
+            <blockquote>
+                {exercise.description}
+            </blockquote>
         </Container>
+    </Container>
     )
 }
 
