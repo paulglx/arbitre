0ddb64a3c985e217b2c8292f19fe4fe1ad44ad0f
paulguilloux-opencell <paul.guilloux@opencellsoft.com>
2022-10-17 10:49:16 +0200

    add Exercise and TestResult pages

--- frontend/src/components/TestResult.tsx
+++ frontend/src/components/TestResult.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useParams } from 'react-router-dom';
+import { useGetSubmissionTestResultsQuery } from '../features/submission/submissionApiSlice'
 
 const TestResult = () => {
 
-    
+    const { id } : any = useParams();
 
-    return (
-        <div>TestResult</div>
+    const {
+        data: testResult,
+        isLoading,
+        isSuccess,
+        isError,
+        error
+    } = useGetSubmissionTestResultsQuery({submission_id:id});
+
+
+    console.log(testResult)
+
+    return (    
+        <></>
     )
 }
 
