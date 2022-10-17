1150840f797dec97bf6c1eaa4d8cd96823707a25
paulguilloux-opencell <paul.guilloux@opencellsoft.com>
2022-10-17 17:25:46 +0200

    Persist login

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,7 +1,31 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, createStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./api/apiSlice";
 import authReducer from "../features/auth/authSlice"
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
+const persistConfig = {
+    key:'main-root',
+    storage,
+}
+
+const persistedAuthReducer = persistReducer(persistConfig, authReducer)
+
+export const store = configureStore({
+    reducer: {
+        [apiSlice.reducerPath]: apiSlice.reducer,
+        auth: persistedAuthReducer
+    },
+    middleware: getDefaultMiddleware =>
+        getDefaultMiddleware().concat(apiSlice.middleware),
+    devTools: true, //TODO remove before prod
+})
+
+export default store
+
+export const Persistor = persistStore(store)
+
+/*
 export const store = configureStore({
     reducer: {
         [apiSlice.reducerPath]: apiSlice.reducer,
@@ -11,3 +35,4 @@ export const store = configureStore({
         getDefaultMiddleware().concat(apiSlice.middleware),
     devTools: true, //TODO remove before prod
 })
+*/
