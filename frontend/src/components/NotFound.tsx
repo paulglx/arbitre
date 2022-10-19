27e26921937b4036ed3f0ae8dd3c41eb438e7152
paulglx <paul.guilloux@telecom-sudparis.eu>
2022-10-19 11:10:33 +0200

    Create 404 page

---
+++ frontend/src/components/NotFound.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+const NotFound = () => {
+  return (<>
+    <div className="d-flex align-items-center justify-content-center vh-100 bg-light">
+          <h1 className="display-1 fw-bold"><a href="/" className='text-decoration-none'>⬅</a> 404</h1>
+    </div>
+</>)
+}
+
+export default NotFound
