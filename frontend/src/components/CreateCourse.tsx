3f4f04c3ba05159c5e315bb59c3ad544c47a40e0
paulguilloux-opencell <paul.guilloux@opencellsoft.com>
2022-10-21 17:54:26 +0200

    Update title dynamically

--- frontend/src/components/CreateCourse.tsx
+++ frontend/src/components/CreateCourse.tsx
@@ -1,25 +1,44 @@
 import React from 'react'
 import { Container, Form } from 'react-bootstrap'
+import { useState } from 'react'
 
 const CreateCourse = () => {
-  return (
-    <Container>
-        <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Course title</Form.Label>
-                <Form.Control type="text" placeholder="Enter title" />
-                <Form.Text className="text-muted">
-                    Give a short title to your course.
-                </Form.Text>
-            </Form.Group>
-        </Form>
 
-        <Form.Group>
-            <Form.Label>Course description</Form.Label>
-            <Form.Control as="textarea" rows={3} />
-        </Form.Group>
-    </Container>
-  )
+    const [title, setTitle] = useState("New course")
+
+    const handleTitleInput = (e:any) => {
+        setTitle(e.target.value ? e.target.value : "New course")
+    }
+
+    return (
+        <Container className="d-flex align-items-center vh-100">
+            <Container>
+
+                <h1>{title}</h1>
+
+                <Form>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Course title</Form.Label>
+                        <Form.Control 
+                            type="text" 
+                            placeholder="Enter title"
+                            onChange={handleTitleInput}
+                            autoComplete='off'
+                            required
+                        />
+                        <Form.Text className="text-muted">
+                            Give a short title to your course.
+                        </Form.Text>
+                    </Form.Group>
+                </Form>
+
+                <Form.Group>
+                    <Form.Label>Course description</Form.Label>
+                    <Form.Control as="textarea" rows={3} />
+                </Form.Group>
+            </Container>
+        </Container>
+    )
 }
 
 export default CreateCourse
