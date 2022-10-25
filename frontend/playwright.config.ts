6ffd81e9e30c96bbd5d281d9ca6d1eeada81fcf2
paulglx <paul.guilloux@telecom-sudparis.eu>
2022-10-25 16:02:19 +0200

    Fix playwright config

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -101,7 +101,7 @@ const config: PlaywrightTestConfig = {
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'cd ../backend && python manage.py runserver && cd ../frontend && npm start',
+    command: 'npm start',
     port: 3000,
     reuseExistingServer: !process.env.CI
   },
