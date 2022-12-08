1940ff864c186c8b88dbf081cc69eee7afa4207c
paulglx <paul.guilloux@telecom-sudparis.eu>
2022-12-08 12:42:56 +0100

    Complete rehaul of results page

--- frontend/src/components/Results.tsx
+++ frontend/src/components/Results.tsx
@@ -1,161 +1,140 @@
-import { Accordion, Badge, Container, Modal, Spinner, Table } from 'react-bootstrap'
+import { Container, Dropdown, Form } from 'react-bootstrap'
+import { useEffect, useState } from 'react'
 
-import Error from './Error'
+import { CaretDownFill } from 'react-bootstrap-icons'
 import Header from './Header'
-import TestResult from './TestResult'
-import { selectIsTeacher } from '../features/auth/authSlice'
-import { useGetAllResultsQuery } from '../features/results/resultsApiSlice'
-import { useSelector } from 'react-redux'
-import { useState } from 'react'
+import React from 'react'
+import ResultsTable from './ResultsTable'
+import { useGetCoursesSessionsExercisesQuery } from '../features/courses/courseApiSlice'
 
 const Results = () => {
 
-    const isTeacher = useSelector(selectIsTeacher)
-
-    const [showModal, setShowModal] = useState(false)
-    const [modalTitle, setModalTitle] = useState("")
-    const [modalContent, setModalContent] = useState(<></>)
-
-    const handleModalClose = () => setShowModal(false)
-    const handleModalShow = () => setShowModal(true)
+    const [currentSession, setCurrentSession] = useState(-1)
+    const [currentSessionTitle, setCurrentSessionTitle] = useState('')
+    const [sessionSearch, setSessionSearch] = useState('')
+    const [sessionSearchResults, setSessionSearchResults] = useState<any>([])
 
     const {
-        data: results,
-        isSuccess,
-        isError,
-        error,
-    } = useGetAllResultsQuery({})
+        data: courses,
+        isSuccess: isCoursesSuccess,
+        isError: isCoursesError,
+        error: coursesError,
+    } = useGetCoursesSessionsExercisesQuery({});
 
-    const courses = results?.courses
-
-    const statusContent = (status:string) => {
-        if (status === "not submitted") {
-            return <span className='text-muted'>-</span>
-        } else if (status === "running") {
-         return (
-            <Spinner animation="border" role="status" as="span" size="sm">
-                <span className="visually-hidden">Running...</span>
-            </Spinner>)
-        } else if (status === "PENDING" || status === "pending") {
-         return (
-            <Spinner animation="border" role="status" as="span" size="sm">
-                <span className="visually-hidden">Pending...</span>
-            </Spinner>)
-        } else if (status === "success") {
-            return <Badge bg="success">Success</Badge>
-        } else if (status === "failed") {
-            return <Badge bg="secondary">Fail</Badge>
-        } else if (status === "error") {
-            return <Badge bg="danger">Error</Badge>
+    useEffect(() => {
+        if (courses) {
+            setCurrentSession(courses[0].sessions[0].id)
+            setCurrentSessionTitle(courses[0].sessions[0].title)
         }
-    }
+    }, [courses])
 
-    const tableHeadContent = (session:any) => {
-        return session.students_data[0]?.results.length > 0 ? (
-            <thead>
-                <tr key={-1}>
-                    <th key={-1}>Student</th>
-                    {session.students_data[0]?.results?.map((exercise:any, i:number) => (
-                        <th className='fw-normal' key={i}>{exercise.exercise_title}</th>
-                    ))}
-                </tr>
-            </thead>
-        ) : (<></>)
-    }
+    useEffect(() => {
+        if (sessionSearch) {
+            const results = courses?.map((course: any) => {
+                return {
+                    course: course,
+                    sessions: course.sessions.filter((session: any) => session.title.toLowerCase().includes(sessionSearch.toLowerCase()))
+                }
+            })
+            setSessionSearchResults(results)
+        } else {
+            const results = courses?.map((course: any) => {
+                return {
+                    course: course,
+                    sessions: course.sessions
+                }
+            })
+            setSessionSearchResults(results)
+        }
+    }, [sessionSearch, courses])
 
-    const tableBodyContent = (session:any) => {
-        console.log(session)
-        return session.students_data[0]?.results.length > 0 ? (<>
-            <tbody className=''>
-                {session.students_data.map((student:any, i:number) => (
-                    <tr key={i}>
-                        <td className=''>{student.student_name}</td>
-                        {student.results?.map((exercise:any, j:number) => (
-                            <td
-                                className='text-center cursor-pointer'
-                                role={exercise.status !== "not submitted" ? "button" : ""}
-                                key={j}
-                                onClick = {exercise.status !== "not submitted" ? (() => {
-                                        setModalTitle(`${student.student_name}'s submission for ${exercise.exercise_title}`)
-                                        setModalContent(<TestResult exercise_id={exercise.exercise_id} user_id={student.student_id} />)
-                                        handleModalShow()
-                                    }) : (
-                                        undefined
-                                    )
+    const CustomToggle = React.forwardRef(({ children, onClick }: any, ref) => (
+        <a
+            className='h2 text-dark px-2 py-1 text-decoration-none shadow-sm border rounded-4'
+            href="#dropdown"
+            ref={ref as any}
+            onClick={(e) => {
+                e.preventDefault();
+                onClick(e);
+            }}
+        >
+            {children} <CaretDownFill />
+        </a>
+    ));
+
+    const CustomMenu = React.forwardRef(
+        ({ children, className, 'aria-labelledby': labeledBy }: any, ref) => {
+
+            return (
+                <div
+                    ref={ref as any}
+                    className={className}
+                    aria-labelledby={labeledBy}
+                >
+                    <Form.Control
+                        autoFocus
+                        className="rounded-3 mx-2 w-75"
+                        placeholder="Type to filter"
+                        onChange={(e) => setSessionSearch(e.target.value)}
+                        value={sessionSearch}
+                    />
+                    <ul className="list-unstyled">
+                        {
+                            sessionSearchResults?.map((course: any) => (<>
+                                <Dropdown.Divider />
+                                {course.sessions.length > 0 ?
+                                    <Dropdown.Header key={course.course.id} className="text-wrap">{course.course.title}</Dropdown.Header>
+                                    : null
                                 }
-                            >
-                                {statusContent(exercise.status)}
-                            </td>
-                        ))}
-                    </tr>
-                ))}
-            </tbody>
-        </>) : (
-            <tbody>
-                <tr>
-                    <td colSpan={session.students_data[0]?.results?.length + 1} className='text-center text-muted'>There are no students registered to this course</td>
-                </tr>
-            </tbody>
-        )
-    }
+                                {course.sessions.map((session: any) => (<>
+                                    <Dropdown.Item
+                                        key={session.id}
+                                        eventKey={session.id}
+                                        className={"text-wrap" + (session.id === currentSession ? " bg-primary text-white" : "")}
+                                        onClick={(eventKey: any) => {
+                                            setCurrentSession(session.id)
+                                            setCurrentSessionTitle(session.title)
+                                        }}
+                                    >
+                                        {session.title}
+                                    </Dropdown.Item>
+                                </>))}
+                            </>))
+                        }
+                    </ul>
+                </div>
+            );
+        },
+    );
 
-    const courseResultsTable = (course:any) => {
-
-        console.log(course)
-
-        return isSuccess ? (
-            course.sessions.map((session:any, i:number) => (<>
-                <Accordion.Item eventKey={String(i)} key={i}>
-                    <Accordion.Header>{session.session_title}</Accordion.Header>
-                    <Accordion.Body className='overflow-scroll p-0 m-0'>
-                        
-                        <Table striped bordered hover className='p-0 m-0'>
-                            {tableHeadContent(session)}
-                            {tableBodyContent(session)}
-                        </Table>
-
-                    </Accordion.Body>
-                </Accordion.Item>
-            </>))) : (<></>)
-    }
-
-    return isTeacher && isSuccess ? (<>
-
-        <Modal show={showModal} onHide={handleModalClose}>
-            <Modal.Header closeButton>
-                <Modal.Title className='h6'>
-                    {modalTitle}
-                </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                {modalContent}
-            </Modal.Body>
-        </Modal>
-
+    return isCoursesSuccess ? (<>
         <Header />
 
+        <br />
         <br />
 
         <Container>
 
-            <h1 className='h2 fw-bold'>Results</h1>
+            <Dropdown>
+                <Dropdown.Toggle as={CustomToggle} id="dropdown-custom-components">
+                    {currentSessionTitle}
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu as={CustomMenu} className="border rounded-4 mt-3 shadow-sm w-50">
+
+                </Dropdown.Menu>
+            </Dropdown>
 
             <br />
 
-            {courses.map((course:any, i:number) => (<>
-
-                <h2>{course.course_title}</h2>
-
-                <Accordion key={course.course_id}>
-                    {courseResultsTable(course)}
-                </Accordion>
-
-                <br />
-                
-            </>))}
+            {currentSession !== -1 ?
+                <ResultsTable session_id={currentSession} />
+                :
+                <></>
+            }
 
         </Container>
-    </>) : (<Error isError={isError} error={JSON.stringify(error)} />)
+    </>) : (<></>)
 }
 
 export default Results
