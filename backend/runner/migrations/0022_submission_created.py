eef8e41b18f8ec46ab5e02fdfc2e6e5a034ffea4
paulglx <paul.guilloux@telecom-sudparis.eu>
2023-01-03 16:17:59 +0100

    Migration

--- backend/runner/migrations/0022_submission_created.py
+++ backend/runner/migrations/0022_submission_created.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('runner', '0021_alter_testresult_stdout'),
+        ("runner", "0021_alter_testresult_stdout"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='submission',
-            name='created',
+            model_name="submission",
+            name="created",
             field=models.DateTimeField(auto_now=True),
         ),
     ]
