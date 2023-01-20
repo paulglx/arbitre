4a737c77b21cbd8e275ff8b0b90a6049f2e916e6
paulglx <paul.guilloux@telecom-sudparis.eu>
2023-01-20 15:52:05 +0100

    change run script

--- run.sh
+++ run.sh
@@ -29,6 +29,6 @@ echo -e "${BLUE} Automated code correction platform ${NC}"
 
     trap _catch ERR
     trap _finally EXIT
-    (trap 'kill 0' SIGINT; cd backend && python manage.py runserver 0.0.0.0:8000 & cd backend && celery -A arbitre worker -l info -B -E & cd frontend && COLOR=1 npm start | cat)
+    (trap 'kill 0' SIGINT; cd backend && python manage.py runserver & cd backend && celery -A arbitre worker -l info -B -E)
     #TODO prod: switch back to (trap 'kill 0' SIGINT; cd backend && python manage.py runserver & cd backend && celery -A arbitre worker -l info -B -E & cd frontend && npm run build && serve -s build)
 )
