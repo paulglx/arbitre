af5a8caa5d50e737fddb298b5ac2565b636f555e
paulglx <paul.guilloux@telecom-sudparis.eu>
2023-01-31 17:27:38 +0100

    Add reauth to "requireauth" component

--- frontend/src/components/RequireAuth.tsx
+++ frontend/src/components/RequireAuth.tsx
@@ -1,15 +1,27 @@
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 
-import { selectCurrentAccessToken } from "../features/auth/authSlice";
+import { selectCurrentKeycloakToken } from "../features/auth/authSlice";
+import { useKeycloak } from "@react-keycloak/web";
 import { useSelector } from "react-redux";
 
 const RequireAuth = () => {
-    const accessToken = useSelector(selectCurrentAccessToken)
+
+    const { keycloak, initialized }: any = useKeycloak()
+    const keycloakToken = useSelector(selectCurrentKeycloakToken)
     const location = useLocation()
 
+    initialized && keycloak.updateToken(5).then((refreshed: boolean) => {
+        if (refreshed) {
+            console.log('Token was successfully refreshed');
+        } else {
+            console.log('Token is still valid');
+        }
+    }).catch(() => {
+        console.log('Failed to refresh token');
+    });
 
     return (
-        accessToken
+        keycloakToken
             ? <Outlet />
             : <Navigate to="/" state={{ from: location }} replace />
     )
