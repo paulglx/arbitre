b16fb643368abc719a6519b143347f94b5b79b82
paulglx <paul.guilloux@telecom-sudparis.eu>
2023-01-31 17:25:58 +0100

    Remove reauth from api query

--- frontend/src/app/api/apiSlice.ts
+++ frontend/src/app/api/apiSlice.ts
@@ -1,53 +1,19 @@
-import { FetchArgs, createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { logOut, setCredentials } from '../../features/auth/authSlice';
-
-import { BaseQueryApi } from '@reduxjs/toolkit/dist/query/baseQueryTypes';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const baseQuery = fetchBaseQuery({
     baseUrl: window.location.protocol + '//' + window.location.hostname,
     credentials: 'include',
     prepareHeaders: (headers: Headers, { getState }: any) => {
-        const accessToken = getState().auth.accessToken
-        if (accessToken) {
-            headers.set("authorization", `Bearer ${accessToken}`)
+        const keycloakToken = getState().auth.keycloakToken
+        if (keycloakToken) {
+            headers.set("authorization", `Bearer ${keycloakToken}`)
         }
         return headers
     }
 })
 
-const baseQueryWithReauth = async (args: string | FetchArgs, api: BaseQueryApi, extraOptions: {}) => {
-    let result = await baseQuery(args, api, extraOptions);
-
-    if (result?.error?.status === 403 || result?.error?.status === 401) {
-        const state: any = api.getState()
-        const refreshResult = await baseQuery({
-            url: '/api/auth/token/refresh/',
-            method: 'POST',
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ "refresh": state.auth.refreshToken })
-        }, api, extraOptions)
-
-        if (refreshResult?.data) {
-            const state: any = api.getState()
-            //store new token
-            api.dispatch(setCredentials({
-                ...refreshResult.data,
-                //keep refresh token, roles and user data
-                refresh: state.auth.refreshToken,
-                user: state.auth.user,
-                roles: state.auth.roles
-            }))
-            //retry original query
-            result = await baseQuery(args, api, extraOptions)
-        } else {
-            api.dispatch(logOut({}))
-        }
-    }
-    return result;
-}
-
 export const apiSlice = createApi({
-    baseQuery: baseQueryWithReauth,
+    baseQuery,
     refetchOnMountOrArgChange: true,
     endpoints: builder => ({})
 })
