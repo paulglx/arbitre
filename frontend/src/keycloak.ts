891a15d00c3192f27ac8f80e1d37ef1f06974dbe
Paul Guilloux <paul.guilloux@telecom-sudparis.eu>
2023-03-24 16:27:43 +0100

    use env in keycloak config

--- frontend/src/keycloak.ts
+++ frontend/src/keycloak.ts
@@ -3,9 +3,9 @@ import Keycloak from 'keycloak-js'
 // Setup Keycloak instance as needed
 // Pass initialization options as required or leave blank to load from 'keycloak.json'
 const keycloak = new Keycloak({
-    url: "http://localhost:8080/",
-    realm: "arbitre",
-    clientId: "arbitre-frontend",
+    url: process.env.REACT_APP_KEYCLOAK_URL,
+    realm: process.env.REACT_APP_KEYCLOAK_REALM || "master",
+    clientId: process.env.REACT_APP_KEYCLOAK_CLIENT_ID || "arbitre",
 });
 
 export default keycloak
