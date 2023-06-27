23f46752bc3afeca7ea966ca704096d1a3d68010
paulglx <paul.guilloux@telecom-sudparis.eu>
2023-06-27 09:54:48 +0200

    Make dropdown interactive

--- frontend/src/components/Dashboard/Dashboard.tsx
+++ frontend/src/components/Dashboard/Dashboard.tsx
@@ -1,6 +1,4 @@
-import 'flowbite'
-
-import { Button, Container, ListGroup } from 'react-bootstrap'
+import { Button, ListGroup } from 'react-bootstrap'
 import { ChevronDown, Search } from "heroicons-react"
 import { useEffect, useState } from 'react'
 
@@ -15,6 +13,14 @@ const Dashboard = () => {
     const [currentSessionTitle, setCurrentSessionTitle] = useState('')
     const [sessionSearch, setSessionSearch] = useState('')
     const [sessionSearchResults, setSessionSearchResults] = useState<any>([])
+    const [dropdownOpen, setDropdownOpen] = useState(false)
+
+    const dropdownButtonRef = React.createRef<HTMLButtonElement>()
+    const dropdownMenuRef = React.createRef<HTMLDivElement>()
+
+    const toggleDropdown = () => {
+        setDropdownOpen(!dropdownOpen)
+    }
 
     const {
         data: courses,
@@ -57,17 +63,18 @@ const Dashboard = () => {
         }
     }, [sessionSearch, courses])
 
-    // TODO make it open and close - https://flowbite.com/docs/components/dropdowns/#dropdown-with-search
-
     const sessionDropdown = () => {
         return (<>
+            <script src="../../../node_modules/flowbite/dist/flowbite.min.js"></script>
             <button
                 className='text-2xl font-bold px-4 py-2 border-4 border-gray-300 bg-gray-100 rounded-md'
-                data-dropdown-placement="bottom-start"
-                data-dropdown-toggle="sessions-dropdown-menu"
                 id="sessions-dropdown-search-button"
                 key={currentSession}
                 type="button"
+                aria-haspopup="true"
+                aria-expanded="true"
+                onClick={toggleDropdown}
+                ref={dropdownButtonRef}
             >
                 {currentSessionTitle ? currentSessionTitle : "Select a session"}
                 <ChevronDown className="inline-block ml-2" />
@@ -76,7 +83,9 @@ const Dashboard = () => {
 
             <div
                 id="sessions-dropdown-menu"
-                className="z-10 w-96 visible mt-2 bg-white border-4 border-gray-300 rounded-md shadow-md"
+                className={"z-10 w-96 mt-2 bg-white border-4 border-gray-300 rounded-md shadow-md " + (dropdownOpen ? "visible" : "hidden")}
+                aria-labelledby="sessions-dropdown-search-button"
+                ref={dropdownMenuRef}
             >
                 <div className="p-3">
                     <label htmlFor="input-group-search" className="sr-only">
@@ -100,37 +109,47 @@ const Dashboard = () => {
 
                 <ul className='h-48 px-3 overflow-y-auto text-sm text-gray-700'>
                     {
-                        sessionSearchResults?.map((course: any) => (<>
-                            {course.sessions.length > 0 ?
+                        sessionSearchResults?.map((course: any) => (
+                            <div key={course.course.id}>
                                 <li key={course.course.id} className="my-2">
                                     <span
+                                        key={course.course.id}
                                         className="px-4 py-2 rounded-md text-gray-700 bg-gray-100 font-bold cursor-default"
                                     >
                                         {course.course.title}
                                     </span>
                                 </li>
-                                : null
-                            }
-                            {course.sessions.map((session: any) => (<>
-                                <li
-                                    key={session.id}
-                                    onClick={() => {
-                                        setCurrentSession(session.id)
-                                        setCurrentSessionTitle(session.title)
-                                    }}
-                                    className='my-4'
-                                >
-                                    <span className={"px-4 py-2 text-sm rounded-md " + (session.id === currentSession ? "cursor-default font-bold bg-gray-700 text-gray-100 " : "cursor-pointer hover:bg-gray-200")}>
-                                        {session.title}
-                                    </span>
-                                </li>
-                            </>))}
-                        </>))
+                                {course.sessions.length > 0 ? course.sessions.map((session: any) => (<div key={session.id}>
+                                    <li
+                                        key={session.id}
+                                        onClick={() => {
+                                            setCurrentSession(session.id)
+                                            setCurrentSessionTitle(session.title)
+                                        }}
+                                        className='my-4'
+                                    >
+                                        <span
+                                            className={"px-4 py-2 text-sm rounded-md " + (session.id === currentSession ? "cursor-default font-bold bg-gray-700 text-gray-100 " : "cursor-pointer hover:bg-gray-200")}
+                                            key={session.id}
+                                        >
+                                            {session.title}
+                                        </span>
+                                    </li>
+                                </div>))
+                                    :
+                                    <li className="my-4">
+                                        <span
+                                            className="px-4 py-2 rounded-md text-gray-500 cursor-default italic"
+                                            key={course.course.id + "-no-sessions"}
+                                        >
+                                            No sessions
+                                        </span>
+                                    </li>
+                                }
+                            </div>))
                     }
-                </ul >
-
-            </div>
-
+                </ul>
+            </div >
         </>)
     }
 
@@ -161,7 +180,7 @@ const Dashboard = () => {
             <br />
             <br />
 
-            <Container>
+            <div className="container mx-auto">
                 <ListGroup>
                     <ListGroup.Item className='p-3 dashed-border rounded-4 text-center text-muted'>
                         <span className='fw-bold'>No sessions</span> <br />
@@ -169,7 +188,7 @@ const Dashboard = () => {
                         <Button className='border mt-2' variant='light' href="/course">← Back to courses</Button>
                     </ListGroup.Item>
                 </ListGroup>
-            </Container>
+            </div>
         </>
     }
 
