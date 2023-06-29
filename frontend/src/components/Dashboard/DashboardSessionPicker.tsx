798d4df80bde56056067adb704f4ec221fd87c48
paulglx <paul.guilloux@telecom-sudparis.eu>
2023-06-29 10:31:40 +0200

    Fix accessibility
    Add accessible label to session search field

--- frontend/src/components/Dashboard/DashboardSessionPicker.tsx
+++ frontend/src/components/Dashboard/DashboardSessionPicker.tsx
@@ -93,9 +93,11 @@ const DashboardSessionPicker = (props: any) => {
                         <MagnifyingGlassIcon id="search-icon" className="w-5 h-5 text-gray-400" />
                     </div>
                     <input
-                        id="input-group-search"
+                        aria-label="Search sessions"
+                        autoComplete="off"
                         autoFocus
                         className="block w-full pl-10 pr-3 py-2 border border-gray-300 rounded-md leading-5 bg-white placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-1 focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                        id="input-group-search"
                         onChange={(e) => setSessionSearch(e.target.value)}
                         placeholder="Search session"
                         type="search"
