e1e1debd27fa4668536d8de460a526c05e29e34d
paulglx <paul.guilloux@telecom-sudparis.eu>
2023-07-03 15:07:42 +0200

    Replace "copied code" message with notification
    - Uses global notification system
    - Prevents layout shift

--- frontend/src/components/Course/Students/StudentsInvite.tsx
+++ frontend/src/components/Course/Students/StudentsInvite.tsx
@@ -1,23 +1,25 @@
 // import "../../../join-code.css"
 
-import { ArrowPathIcon, CheckCircleIcon } from '@heroicons/react/24/outline';
 import { useRefreshJoinCodeMutation, useSetJoinCodeEnabledMutation } from "../../../features/courses/courseApiSlice"
 
+import { ArrowPathIcon } from '@heroicons/react/24/outline';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { IoToggleOutline } from 'react-icons/io5';
+import { pushNotification } from '../../../features/notification/notificationSlice';
 import { selectCurrentUser } from "../../../features/auth/authSlice"
+import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux'
 import { useState } from 'react'
 
 const StudentsInvite = (props: any) => {
 
-  const username = useSelector(selectCurrentUser)
-  const course = props.course
-  const [copied, setCopied] = useState(false)
-  const [refreshJoinCode] = useRefreshJoinCodeMutation()
-  const [joinCodeEnabled, setJoinCodeEnabled] = useState(props.course.join_code_enabled)
-  const [joinCode, setJoinCode] = useState(props.course.join_code)
   const [enableJoinCode] = useSetJoinCodeEnabledMutation()
+  const [joinCode, setJoinCode] = useState(props.course.join_code)
+  const [joinCodeEnabled, setJoinCodeEnabled] = useState(props.course.join_code_enabled)
+  const [refreshJoinCode] = useRefreshJoinCodeMutation()
+  const course = props.course
+  const dispatch = useDispatch()
+  const username = useSelector(selectCurrentUser)
 
   const ownersUsernames = course?.owners.map((owner: any) => owner.username);
   const isOwner = ownersUsernames?.includes(username);
@@ -42,10 +44,10 @@ const StudentsInvite = (props: any) => {
   }
 
   const handleCopyJoinCode = () => {
-    setCopied(true);
-    setTimeout(() => {
-      setCopied(false);
-    }, 1000);
+    dispatch(pushNotification({
+      message: "Copied join code",
+      type: "info"
+    }));
   };
 
   return isOwner || isTutor ? (
@@ -59,7 +61,7 @@ const StudentsInvite = (props: any) => {
             <CopyToClipboard text={joinCode} onCopy={handleCopyJoinCode}>
               <button
                 id="join-code"
-                className="user-select-none text-center px-2 py-1 rounded-3xl text-4xl"
+                className="font-mono user-select-none text-center px-2 py-1 rounded-3xl text-4xl"
               >
                 {joinCode}
               </button>
@@ -67,9 +69,9 @@ const StudentsInvite = (props: any) => {
           ) : (
             <span
               id="join-code"
-              className="user-select-none text-center px-2 py-0 rounded-4 h-1"
+              className="font-mono user-select-none text-center px-2 py-1 rounded-3xl text-4xl"
             >
-              &nbsp;
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             </span>
           )}
         </div>
@@ -103,20 +105,12 @@ const StudentsInvite = (props: any) => {
 
       {isOwner ? (
         <>
-          <div className="flex items-center justify-center">
-            {copied && (
-              <div className="flex items-center">
-                <CheckCircleIcon className="h-5 w-5 text-gray-500 mr-2" />
-                <p className="text-gray-500">Code copied to clipboard!</p>
-              </div>
-            )}
-          </div>
-          <p className="text-muted hidden md:block">
+          <span className="text-muted hidden md:block">
             Students can click "Join course" on the homepage and enter this code.
-          </p>
+          </span>
           {joinCodeEnabled ? (
             <>
-              <div className="flex items-center md:block">
+              <span className="flex items-center md:block">
                 <span className="mr-2">Join link:</span>
                 <span
                   className="text-decoration-underline text-primary user-select-all text-blue-500"
@@ -128,7 +122,7 @@ const StudentsInvite = (props: any) => {
                 >
                   {window.location.origin}/course/join/{joinCode}
                 </span>
-              </div>
+              </span>
             </>
           ) : (
             <span>The join link is disabled.</span>
