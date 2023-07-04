845dd1bc2642e6f1328dc78f08b70e659369de05
paulglx <paul.guilloux@telecom-sudparis.eu>
2023-07-04 18:12:51 +0200

    fix: missing key in breadcrump list items

--- frontend/src/components/Common/Breadcrumb.tsx
+++ frontend/src/components/Common/Breadcrumb.tsx
@@ -19,22 +19,22 @@ const Breadcrumb = ({ items }: BreadcrumbProps) => {
     return (
         <nav className="flex px-5 py-3 mt-2 md:mt-6 w-full text-gray-700 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700" aria-label="Breadcrumb">
             <ol className="flex items-center space-x-1">
-                {items.map((item, i) => (<>
+                {items.map((item, i) => (
                     <li key={i} className="flex items-center">
                         {item.link ? (
-                            <Link to={item.link} className="flex items-center text-gray-700 hover:text-blue-600">
+                            <Link key={i} to={item.link} className="flex items-center text-gray-700 hover:text-blue-600">
                                 {truncateIfLong(item.title, 20)}
                             </Link>
                         ) : (
-                            <span className="flex items-center text-gray-500" aria-current="page">
+                            <span key={i} className="flex items-center text-gray-500" aria-current="page">
                                 {item.title}
                             </span>
                         )}
+                        {i < items.length - 1 ? (
+                            <ChevronRightIcon className="flex-shrink-0 w-4 h-4 ml-1 text-gray-400" aria-hidden="true" />
+                        ) : (<></>)}
                     </li>
-                    {i < items.length - 1 ? (
-                        <ChevronRightIcon className="flex-shrink-0 w-4 h-4 text-gray-400" aria-hidden="true" />
-                    ) : (<></>)}
-                </>))}
+                ))}
             </ol>
         </nav >
     )
