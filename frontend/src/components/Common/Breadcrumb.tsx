7437fdffa7362e5adb69433a382cc422f3a20571
paulglx <paul.guilloux@telecom-sudparis.eu>
2023-07-06 14:34:27 +0200

    feature: turn breadcrumb to back button on mobile

--- frontend/src/components/Common/Breadcrumb.tsx
+++ frontend/src/components/Common/Breadcrumb.tsx
@@ -1,8 +1,9 @@
-import { ChevronRightIcon } from "@heroicons/react/24/solid"
+import { ArrowLeftIcon, ChevronRightIcon } from "@heroicons/react/24/solid"
+
 import { Link } from "react-router-dom"
 
 export interface BreadcrumbItem {
-    link: string | null,
+    link?: string | null,
     title: string
 }
 
@@ -16,8 +17,8 @@ const Breadcrumb = ({ items }: BreadcrumbProps) => {
         return text.length > length ? text.substring(0, length) + "..." : text;
     }
 
-    return (
-        <nav className="flex px-5 py-3 mt-2 md:mt-6 w-full text-gray-700 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700" aria-label="Breadcrumb">
+    return (<>
+        <nav className="hidden md:flex py-3 mt-2 md:mt-6 w-full text-gray-700 rounded-lg dark:bg-gray-800 dark:border-gray-700" aria-label="Breadcrumb">
             <ol className="flex items-center space-x-1">
                 {items.map((item, i) => (
                     <li key={i} className="flex items-center">
@@ -26,7 +27,7 @@ const Breadcrumb = ({ items }: BreadcrumbProps) => {
                                 {truncateIfLong(item.title, 20)}
                             </Link>
                         ) : (
-                            <span key={i} className="flex items-center text-gray-500" aria-current="page">
+                            <span key={i} className="bg-blue-50 px-4 py-2 rounded-lg flex items-center text-blue-600" aria-current="page">
                                 {item.title}
                             </span>
                         )}
@@ -37,7 +38,13 @@ const Breadcrumb = ({ items }: BreadcrumbProps) => {
                 ))}
             </ol>
         </nav >
-    )
+        <div className="flex md:hidden">
+            <Link className="flex bg-gray-50 border px-4 py-2 rounded-lg items-center text-gray-600 hover:bg-gray-100" to={items[items.length - 2].link || ""}>
+                <ArrowLeftIcon className="flex-shrink-0 w-4 h-4 mr-2 text-gray-600" aria-hidden="true" />
+                {items[items.length - 2].title}
+            </Link>
+        </div>
+    </>)
 }
 
 export default Breadcrumb
