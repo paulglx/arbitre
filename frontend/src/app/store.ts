4bd6439f13d63f5f678d1daf0266e6789976caf0
paulglx <paul.guilloux@telecom-sudparis.eu>
2023-07-22 18:18:05 +0200

    fix: data remaining after delete

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -8,7 +8,8 @@ import storage from "redux-persist/lib/storage";
 
 const persistConfig = {
     key: 'main-root',
-    storage
+    storage,
+    whitelist: ['auth'],
 }
 
 const persistedAuthReducer = persistReducer(persistConfig, authReducer)
