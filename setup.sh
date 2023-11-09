d66aa2ee665326c34d6165f7883aabbcedbba418
paulglx <paul.guilloux@telecom-sudparis.eu>
2023-11-09 16:47:49 +0100

    feat: add error message to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 BLUE='\033[0;34m'
+RED='\033[0;31m'
 NC='\033[0m' # No Color
 
 cat << "EOF"
@@ -22,16 +23,12 @@ echo ""
     set -Ee
 
     function _catch {
-        echo "Error: $1"
+        echo -e "${RED}Arbitre couldn't setup correctly"
+        echo -e "${BLUE}Tip: Make sure Keycloak is up and running and .env file is correct${NC}"
         exit 0  # optional; use if you don't want to propagate (rethrow) error to outer shell
-    }
-    function _finally {
-        echo -e "
-${BLUE}Setup complete. Run './run.sh' to start Arbitre.${NC}"
     }
 
     trap _catch ERR
-    trap _finally EXIT
 
     echo "Installing dependencies..."
     cd frontend
@@ -49,4 +46,7 @@ ${BLUE}Setup complete. Run './run.sh' to start Arbitre.${NC}"
 
     cd ..
     chmod +x run.sh
+
+    echo -e "
+${BLUE}Setup complete. Run './run.sh' to start Arbitre.${NC}"
 )
