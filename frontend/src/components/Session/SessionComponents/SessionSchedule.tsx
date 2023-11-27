9e19f8124f22028874162522bb52b3620f179058
paulglx <paul.guilloux@telecom-sudparis.eu>
2023-11-27 12:53:37 +0100

    feat: Fix session schedule radio button styling

--- frontend/src/components/Session/SessionComponents/SessionSchedule.tsx
+++ frontend/src/components/Session/SessionComponents/SessionSchedule.tsx
@@ -164,7 +164,12 @@ const SessionSchedule = (props: any) => {
 
         </div>
 
-        <div id="deadline_type_radio" className="p-2">
+        <div
+            id="deadline_type_radio"
+            className={`p-2
+            ${deadline ? "" : "pointer-events-none opacity-50"}
+            `}
+        >
 
             <label
                 id="deadline_type_label"
@@ -183,6 +188,7 @@ const SessionSchedule = (props: any) => {
                 <div className="flex items-center h-5">
                     <input
                         id="deadline_type"
+                        aria-label="Deadline Type : Soft"
                         name="deadline_type"
                         type="radio"
                         className="focus:ring-blue-500 h-4 w-4 text-blue-600 border-gray-300"
@@ -207,6 +213,7 @@ const SessionSchedule = (props: any) => {
                 <div className="flex items-center h-5">
                     <input
                         id="deadline_type"
+                        aria-label="Deadline Type : Hard"
                         name="deadline_type"
                         type="radio"
                         className="focus:ring-blue-500 h-4 w-4 text-blue-600 border-gray-300"
