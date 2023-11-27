dc0565b5283dd6c6c5671d8c61b77876ecc914e8
paulglx <paul.guilloux@telecom-sudparis.eu>
2023-11-27 11:59:16 +0100

    Add clock icon and display submission deadline

--- frontend/src/components/Exercise/Submission/SubmissionDeadline.tsx
+++ frontend/src/components/Exercise/Submission/SubmissionDeadline.tsx
@@ -1,4 +1,6 @@
-import { ExclamationTriangleIcon } from '@heroicons/react/24/outline';
+import { ClockIcon, ExclamationTriangleIcon } from '@heroicons/react/24/outline';
+
+import moment from 'moment';
 
 const SubmissionDeadline = (props: any) => {
 
@@ -11,16 +13,30 @@ const SubmissionDeadline = (props: any) => {
         </div>)
     }
 
+    if (!exercise?.session?.deadline) {
+        return (<></>)
+    }
+
 
     if (exercise?.session?.has_ended) {
         if (exercise?.session?.can_submit) {
-            return (Warning("The session has ended. You can still submit your solution, but it will be marked as late."))
+            return (Warning(`The session has ended ${moment(exercise.session.deadline).fromNow()}. You can still submit your solution, but it will be marked as late.`))
         } else {
-            return (Warning("The session has ended. You can no longer submit your solution."))
+            return (Warning(`The session has ended ${moment(exercise.session.deadline).fromNow()}. You can no longer submit your solution.`))
         }
     }
 
-    return (<></>)
+    return (<div>
+        <div className='px-2 py-1 mt-2 text-gray-600 flex flex-row items-center'>
+            <ClockIcon className='h-4 w-4' />
+            <p className='ml-1 text-sm'>
+                You have
+                <span className='font-semibold'> {moment(exercise.session.deadline).fromNow(true)} </span>
+                left to submit your solution.
+                <span className='text-gray-400'> Deadline: {moment(exercise.session.deadline).format("LLL")} </span>
+            </p>
+        </div>
+    </div>)
 }
 
 export default SubmissionDeadline
