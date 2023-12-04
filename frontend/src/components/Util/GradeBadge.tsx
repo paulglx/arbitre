7d538cf67570ad131b6fb0cf4f65e246df12c9a3
paulglx <paul.guilloux@telecom-sudparis.eu>
2023-12-04 16:49:46 +0100

    Refactor GradeBadge component to support late grades

--- frontend/src/components/Util/GradeBadge.tsx
+++ frontend/src/components/Util/GradeBadge.tsx
@@ -2,18 +2,18 @@ const round = (value: number, decimals: number) => {
     return Number(Math.round(Number(value + "e" + decimals)) + "e-" + decimals);
 }
 
-const GradeBadge = (props: { grade: number, total: number }) => {
+const GradeBadge = (props: { grade: number, total: number, late?: boolean }) => {
     if (props.total === null)
         return null;
 
     return (<span className="text-xs font-medium px-2">
         <span
-            className=" text-blue-800"
+            className={`${props.late ? "text-amber-600" : "text-blue-800"}`}
         >
             {round(props.grade, 2)}
         </span>
         &nbsp;
-        <span className="text-blue-800 opacity-50">
+        <span className={`${props.late ? "text-amber-600" : "text-blue-800"} opacity-75`}>
             /&nbsp;
             {round(props.total, 2) || "-"}
         </span>
