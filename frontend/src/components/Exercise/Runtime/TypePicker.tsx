2937ba4d0845a81c83030b4b35c5a8b6feea8853
paulglx <paul.guilloux@telecom-sudparis.eu>
2024-02-14 17:09:17 +0100

    feat: Multifile exercise: Warn the user that default course lang is overriden

--- frontend/src/components/Exercise/Runtime/TypePicker.tsx
+++ frontend/src/components/Exercise/Runtime/TypePicker.tsx
@@ -1,5 +1,6 @@
 import { DocumentTextIcon, FolderOpenIcon } from '@heroicons/react/20/solid';
 
+import { InformationCircleIcon } from '@heroicons/react/24/outline';
 import { pushNotification } from '../../../features/notification/notificationSlice';
 import { useDispatch } from 'react-redux';
 import { useUpdateExerciseMutation } from '../../../features/courses/exerciseApiSlice';
@@ -10,10 +11,10 @@ const TypePicker = (props: any) => {
     const [updateExercise] = useUpdateExerciseMutation();
     const dispatch = useDispatch();
 
-    const enabledStyle = "outline outline-2 shadow-lg"
+    const enabledStyle = "outline outline-2 shadow-lg cursor-default"
     const singleEnabledStyle = "bg-blue-50 border-blue-300 outline-blue-400 shadow-blue-100"
     const multiEnabledStyle = "bg-indigo-50 border-indigo-300 outline-indigo-400 shadow-indigo-100"
-    const disabledStyle = "bg-gray-50 border-gray-300 outline-gray-400 shadow-gray-100 border-dashed"
+    const disabledStyle = "bg-gray-50 border-gray-300 outline-gray-400 shadow-gray-100 border-dashed cursor-pointer hover:bg-gray-100 hover:border-solid"
 
     const handleSwitchType = async (e: any) => {
 
@@ -38,6 +39,17 @@ const TypePicker = (props: any) => {
             })
     }
 
+    const MultipleFileInfo = () => {
+        return (<span className='inline-flex flex-row items-center mt-4 text-sm text-gray-500'>
+            <InformationCircleIcon className='hidden md:block size-8 bg-indigo-100 text-indigo-500 rounded-full p-1 mr-2' />
+            <span>
+                <span className='font-semibold text-indigo-500'>Multiple Files mode:&nbsp;</span>
+                The default course language is overriden on this exercise.<br /> Specify compilation and execution with&nbsp;
+                <span className='font-semibold'>Teacher Files</span> below.
+            </span>
+        </span>)
+    }
+
     return (
         <div className="p-1 mb-4">
 
@@ -51,7 +63,7 @@ const TypePicker = (props: any) => {
             <div className='border rounded-xl'>
                 <div className='flex flex-col md:flex-row gap-2 p-2'>
                     <button
-                        className={`px-4 py-2 border basis-1/2 rounded-lg ${type === "single" ? enabledStyle + " " + singleEnabledStyle : disabledStyle} `}
+                        className={`px-4 py-2 border basis-1/2 rounded-lg transition-all ease-in-out ${type === "single" ? enabledStyle + " " + singleEnabledStyle : disabledStyle} `}
                         onClick={handleSwitchType}
                         disabled={type === "single"}
                     >
@@ -63,8 +75,9 @@ const TypePicker = (props: any) => {
                     </button>
 
                     <button
-                        className={`px-4 py-2 border basis-1/2 rounded-lg ${type === "multiple" ? enabledStyle + " " + multiEnabledStyle : disabledStyle} `}
+                        className={`px-4 py-2 border basis-1/2 rounded-lg transition-all ease-in-out ${type === "multiple" ? enabledStyle + " " + multiEnabledStyle : disabledStyle} `}
                         onClick={handleSwitchType}
+                        disabled={type === "multiple"}
                     >
                         <h3 className={`flex flex-row items-center justify-center font-bold ${type === "multiple" ? "text-indigo-600" : "text-gray-400"}`}>
                             <FolderOpenIcon className='size-4 mr-1' />
@@ -74,6 +87,8 @@ const TypePicker = (props: any) => {
                     </button>
                 </div>
             </div>
+
+            {type === "multiple" ? <MultipleFileInfo /> : null}
         </div>
     )
 }
