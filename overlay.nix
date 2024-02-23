82829da1cc2d81e5ce5e72bf64437300da4a8a5f
paulglx <paul.guilloux@telecom-sudparis.eu>
2024-02-23 12:50:47 +0100

    feat: Add djangorestframework-api-key

--- overlay.nix
+++ overlay.nix
@@ -1,47 +1,36 @@
 final: prev: {
-  /* my_python = prev.python3.override {
-    packageOverrides = self: super: {
-      django = super.django_4.override { };
-      django-cors-headers = super.django-cors-headers.override { django = self.django; };
-      djangorestframework = super.djangorestframework.override { django = self.django; };
-      djangorestframework-simplejwt = super.djangorestframework-simplejwt.override { django = self.django; djangorestframework = self.djangorestframework; };
-      moto = super.moto.overridePythonAttrs { doCheck = false; };
-      celery = super.celery.overridePythonAttrs { doCheck = false; };
-      mozilla-django-oidc = self.buildPythonPackage rec {
-        pname = "mozilla-django-oidc";
-        version = "3.0.0";
-        src = self.fetchPypi {
-          inherit pname version;
-          sha256 = "sha256-p9RHr4PLWqFnGiQAmwzmsvDSWenFjYyIx6jQwnwFwE0=";
-        };
-        propagatedBuildInputs = [
-          self.django_4
-          self.josepy
-          self.requests
-          self.cryptography
-        ];
-
-        doCheck = false;
-      };
-    };
-  }; */
   my_python = prev.python3.override {
     packageOverrides = self: super: {
-      mozilla-django-oidc = self.buildPythonPackage rec {
-        pname = "mozilla-django-oidc";
-        version = "4.0.0";
-        src = self.fetchPypi {
-          inherit pname version;
-          sha256 = "sha256-frnQWgM6xhp06jvjPT+CKBi8jcq4HEcf75TA1lx8vhw=";
-        };
-        propagatedBuildInputs = [
-          self.josepy
-          self.requests
-          self.cryptography
-        ];
+      mozilla-django-oidc = self.buildPythonPackage
+        rec {
+          pname = "mozilla-django-oidc";
+          version = "4.0.0";
+          src = self.fetchPypi {
+            inherit pname version;
+            sha256 = "sha256-frnQWgM6xhp06jvjPT+CKBi8jcq4HEcf75TA1lx8vhw=";
+          };
+          propagatedBuildInputs = [
+            self.josepy
+            self.requests
+            self.cryptography
+          ];
 
-        doCheck = false;
-      };
+          doCheck = false;
+        };
+      djangorestframework-api-key = self.buildPythonPackage
+        rec {
+          pname = "djangorestframework-api-key";
+          version = "3.0.0";
+          src = self.fetchPypi {
+            inherit pname version;
+            sha256 = "sha256-8YzfpFquoQ/U2q6/+mBIHOQALJue9sVR7x/CHa3yiEU=";
+          };
+          propagatedBuildInputs = [
+            self.django
+            self.djangorestframework
+          ];
+          doCheck = false;
+        };
     };
   };
 }
