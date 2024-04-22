3b19cf535a1d5dd17ad6a5fd8ecab22c91269c52
paulglx <paul.guilloux@telecom-sudparis.eu>
2024-04-22 09:04:07 +0200

    feat: add cache to django

--- backend/arbitre/settings.py
+++ backend/arbitre/settings.py
@@ -140,6 +140,12 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379",
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
