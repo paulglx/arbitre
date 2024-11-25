3e260bafa13247d324bb40c4e6f38117abade740
paulglx <paul.guilloux@telecom-sudparis.eu>
2024-11-25 15:19:52 +0100

    feat: round grades up to 1 decimal (display only)

--- frontend/src/components/Util/GradeBadge.tsx
+++ frontend/src/components/Util/GradeBadge.tsx
@@ -1,23 +1,23 @@
 const round = (value: number, decimals: number) => {
-    return Number(Math.round(Number(value + "e" + decimals)) + "e-" + decimals);
+  return Number(Math.round(Number(value + "e" + decimals)) + "e-" + decimals);
 }
 
 const GradeBadge = (props: { grade: number, total: number, late?: boolean }) => {
-    if (props.total === null)
-        return null;
+  if (props.total === null)
+    return null;
 
-    return (<span className="text-xs font-medium px-2">
-        <span
-            className={`${props.late ? "text-amber-600" : "text-blue-800"}`}
-        >
-            {round(props.grade, 2)}
-        </span>
-        &nbsp;
-        <span className={`${props.late ? "text-amber-600" : "text-blue-800"} opacity-75`}>
-            /&nbsp;
-            {round(props.total, 2) || "-"}
-        </span>
+  return (<span className="text-xs font-medium px-2">
+    <span
+      className={`${props.late ? "text-amber-600" : "text-blue-800"}`}
+    >
+      {round(props.grade, 1)}
+    </span>
+    &nbsp;
+    <span className={`${props.late ? "text-amber-600" : "text-blue-800"} opacity-75`}>
+      /&nbsp;
+      {round(props.total, 1) || "-"}
+    </span>
 
-    </span>)
+  </span>)
 }
 export default GradeBadge
